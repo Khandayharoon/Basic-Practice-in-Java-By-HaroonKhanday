68b48d7218350390289e6f0bf8708a03dbc88705
Khandayharoon <136074700+Khandayharoon@users.noreply.github.com>
2023-10-16 14:14:54 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,19 @@
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		
+		int a = 60 , b = 80;
+		if(a++ > 60 && b++ >80)
+		{
+			System.out.println("Inside if");
+		}
+		else
+		{
+			System.out.println("Inside else");
+		}
+		System.out.println("a is" + " "+ a + " "+ "b is" + " "+ b);
+
+	}
+
+}
