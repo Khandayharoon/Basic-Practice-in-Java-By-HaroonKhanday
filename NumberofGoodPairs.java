d8f117288b55c6b228fc059a5e51828dda73cdfa
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2023-11-20 05:49:53 -0800

    Add files via upload

---
+++ NumberofGoodPairs.java
@@ -0,0 +1,31 @@
+package ArraysAndLogic.com;
+
+public class NumberofGoodPairs {
+
+	public static void main(String[] args) {
+
+//		int[] nums = { 1, 2, 3, 1, 1, 3 };
+		int[] nums = { 1, 1, 1, 1};
+		numIdenticalPairs(nums);
+	}
+
+	public static int numIdenticalPairs(int[] nums) {
+		int identicalParis= 0;
+		int n = nums.length;
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++)
+			{
+				if(nums[i]==nums[j]) {
+					identicalParis++;
+					System.out.println("("+" "+nums[i]+","+" "+nums[j]+")");
+				}
+			}
+		}
+			
+		
+		return 0;
+		
+		
+
+	}
+}
